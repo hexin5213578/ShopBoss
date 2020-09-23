bdadd11e6516141f699588618f0043de98f545e4
root <1850801419@qq.com>
2020-09-23 21:26:56 +0800

    111

--- app/src/main/java/com/QiSheng/ShopBoss/MainActivity.java
+++ app/src/main/java/com/QiSheng/ShopBoss/MainActivity.java
@@ -1,14 +1,53 @@
 package com.QiSheng.ShopBoss;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.core.app.ActivityCompat;
+
+import com.QiSheng.ShopBoss.base.BaseAvtivity;
+import com.QiSheng.ShopBoss.base.BasePresenter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseAvtivity {
+
+
+    @BindView(R.id.bt_getphone)
+    Button btGetphone;
+
+    @Override
+    protected int getResId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void getData() {
+        btGetphone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected BasePresenter initPresenter() {
+        return null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
